427442ede8d022d70e4421ecf4010e80703465ac
YukiEtoh <snowy0208@icloud.com>
2019-05-14 17:02:59 -0400

    add new file

--- XYphase.py
+++ XYphase.py
@@ -2,27 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+
 for BB_index in range(4):
-    BBlist = 'BB' + `BB_index + 1`   
+    BBlist = 'BB' + `BB_index + 1`
     prefix = BBlist + '-SPW0-DA59'
-    XYCcom = np.load(prefix+'.XYC.npy')
-    XYC = np.imag(XYCcom)
+    XYC = np.imag(np.load(prefix+'.XYC.npy'))
     Y= XYC[0,0:840]
-    X = np.load(prefix+'.TS.npy')/86400 #sec(MJD)->day(MJD)
-
-    #MJD->#Gregori
-    n=X+678881
-    a=4*n+3+4*(3/4*(4*(n+1)/(146097)+1))
-    b=5*(a%1461)/4+2
-    y=a/1461
-    m=b/153+3
-    d=(b%153)/5
-    #print(y,m,d)
-
-    plt.title('XYphase stability',)
-    plt.plot(d,Y,"ro" ,label=BBlist)
-    plt.legend(loc='upper right')
-    plt.xlabel("time")
-    plt.ylabel("XY img")
-    plt.grid()
-    plt.show()
+    X = np.load(prefix+'.TS.npy')
+    if BB_index == 0: 
+       plt.plot(X,Y,"ro" ,label=BBlist)
+    elif BB_index == 1:
+       plt.plot(X,Y,"bo" ,label=BBlist)
+    elif BB_index == 2:
+       plt.plot(X,Y,"go" ,label=BBlist)
+    else:
+       plt.plot(X,Y,"yo" ,label=BBlist)
+plt.title('XYphase stability')
+plt.legend(loc='upper right')
+plt.xlabel("time")
+plt.ylabel("XY imaginary part")
+plt.grid()
+plt.show()
