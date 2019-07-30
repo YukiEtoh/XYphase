05ec070d6eb79f783172f6708d3de1f6d9a0394b
YukiEtoh <snowy0208@icloud.com>
2019-07-30 15:40:55 -0400

    allanvariance2.py

--- allanvariance2.py
+++ allanvariance2.py
@@ -5,24 +5,26 @@ import matplotlib.dates as mdates
 from matplotlib import dates as mdates
 import math
 from datetime import datetime as dt
-MJDsec=np.load('BB1-SPW0-DA59.TS.npy')
-XYphase=np.imag(np.load('BB1-SPW0-DA59.XYC.npy'))[0,0:840]
-scanTotal=np.round(np.sum(MJDsec))
-def allanvar_conversion_counter(startPoint,timeDiff):
-    deltaT1=np.round(time[startPoint+timeDiff]-time[startPoint])
-    deltaT2=np.round(time[startPoint+2*timeDiff]-time[startPoint+timeDiff])
-    if deltaT1 and deltaT2 == 2*timeDiff:
+dataSize=840
+scanTime=np.load('BB1-SPW0-DA59.TS.npy')
+XYphase=np.imag(np.load('BB1-SPW0-DA59.XYC.npy'))[0,0:dataSize]
+scanTimeDiff=np.int(scanTime[dataSize-1]-scanTime[0])
+deltaTRange=np.int(scanTime[dataSize-1]-scanTime[0])/2
+
+def allanvar_conversion_counter(startPoint,nextPoint):   #連続した3点を取れたらカウントする関数
+    deltaT1=np.int(scanTime[startPoint+nextPoint*1]-scanTime[startPoint])
+    deltaT2=np.int(scanTime[startPoint+nextPoint*2]-scanTime[startPoint+nextPoint*1])
+    if deltaT1 == deltaT2 :
        return 1
     else:
        return 0
-def N(tau):
-    vecSize=len(XYphase); y1=XYphase[i]; y2=XYphase[i+tau]; y3=XYphase[i+2*tau]
+def Total_threePoints(startPoint,nextPoint):  #連続した3点を足し合わせる関数
+    vecSize=len(XYphase); y1=XYphase[startPoint]; y2=XYphase[startPoint+nextPoint*1]; y3=XYphase[startPoint+nextPoint*2]
     return (y3-y2-(y2-y1))**2
-
    
-def graph():
-    X=2*deltaT
-    Y=Allanvariance
+def allanvar_graph(): #グラフをプロット
+    X=NP
+    Y=N/NP/2
     plt.loglog(X,Y,"ro",markersize=3)
     plt.title('G31.41+0_a_06_TE/Xb7d0ee BB1')
     plt.xlabel("time lag [s]")
@@ -30,17 +32,21 @@ def graph():
    # a=2
    # b=0.0011488759243233671
    # plt.loglog(a,-2+2b+a'k')
+print("start point","next point","total of three point")
 
-
-for deltaT in range(1,4183211173150):
-    total=0
-    NumOfData=datasize/deltaT-2
-    for i in range(1,NumOfData):  #始点の決定
-        if w(i,deltaT)== 1:
-           total+=N(deltaT)
+for NP in range(1,838):  #次点の決定
+    N=0
+    R=838/NP 
+    for SP in range(1,R): #始点の決定
+        #print(SP)
+        if allanvar_conversion_counter(SP,NP) == 1:
+           #print(Total_threePoints(SP,NP))
+           N+=Total_threePoints(SP,NP)
+           Allanvariance=(N/2)/2
+          # print(SP,NP,N)
         else:
            pass
-    Allanvariance=(total/deltaT)/2
-#    print(Allanvariance,deltaT)
-    graph()
-
+    print(SP,NP,N,N/NP/2)
+       # print(SP,NP,N)
+       # Allanvariance=(N/2)/2
+    allanvar_graph()
