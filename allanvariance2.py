f15b3a3c25deddcf37fda9b14a8230237ba7da23
YukiEtoh <snowy0208@icloud.com>
2019-08-05 14:07:03 -0400

    allanvariance2.py

--- allanvariance2.py
+++ allanvariance2.py
@@ -5,29 +5,19 @@ import matplotlib.dates as mdates
 from matplotlib import dates as mdates
 import math
 from datetime import datetime as dt
-
-timeStamp=np.load('BB1-SPW0-DA59.TS.npy')
-scaledTime = np.round((timeStamp-np.min(timeStamp))/np.min(np.diff(timeStamp)))  
-dataSize=len(timeStamp)
-XYphase=np.imag(np.load('BB1-SPW0-DA59.XYC.npy'))[0,0:dataSize]
-TotalScanTime=np.int(scaledTime[dataSize-1]-scaledTime[0])
-
 def allanvar_conversion_counter(startPoint,timeInterval):   #連続した3点を取れたらカウントする関数
-    nextPoint=np.where(np.round(scaledTime[startPoint]+timeInterval)==np.round(scaledTime))[0]
-    thirdPoint=np.where(np.round(scaledTime[startPoint]+timeInterval*2)==np.round(scaledTime))[0]
+    nextPoint=np.where(scaledTime[startPoint]+timeInterval==scaledTime)[0]
+    thirdPoint=np.where(scaledTime[startPoint]+timeInterval*2==scaledTime)[0]
     if nextPoint and thirdPoint :
        return 1
     else:
        return 0
-
 def Total_threePoints(startPoint,timeInterval):  #連続した3点を足し合わせアラン分散を計算する関数
-    nextPoint=np.where(np.round(scaledTime[startPoint]+timeInterval)==np.round(scaledTime))[0]
-    thirdPoint=np.where(np.round(scaledTime[startPoint]+timeInterval*2)==np.round(scaledTime))[0]
-    y1=XYphase[startPoint]; y2=XYphase[int(nextPoint)]; y3=XYphase[int(thirdPoint)]
+    nextPoint=np.where(scaledTime[startPoint]+timeInterval==scaledTime)[0]
+    thirdPoint=np.where(scaledTime[startPoint]+timeInterval*2==scaledTime)[0]
+    y1=XYimagPart[startPoint]; y2=XYimagPart[int(nextPoint)]; y3=XYimagPart[int(thirdPoint)]
     threePoints=(y3-2*y2-y1)**2
     return threePoints
-    
-
 def allanvar_graph(): #グラフをプロット
     X=TI
     Y=allanvariance
@@ -38,6 +28,13 @@ def allanvar_graph(): #グラフをプロット
     a=np.array([  2,   10,  100, 1000],dtype=float)
     b=5.8e-6
     plt.loglog(a,a**(-2)*b,'k')
+
+timeStamp=np.load('BB1-SPW0-DA59.TS.npy')
+scaledTime = np.round((timeStamp-np.min(timeStamp))/np.min(np.diff(timeStamp)))
+dataSize=len(timeStamp)
+XYimagPart=np.imag(np.load('BB1-SPW0-DA59.XYC.npy'))[0,0:dataSize]
+TotalScanTime=np.int(scaledTime[dataSize-1]-scaledTime[0])
+
 
 for TI in range(1,TotalScanTime):  #次点の決定
           Total=0
