70bd25befebd7401f30dae67ff3da820c695dd3b
YukiEtoh <snowy0208@icloud.com>
2019-08-06 15:06:00 -0400

    allanvariance2.py

--- allanvariance2.py
+++ allanvariance2.py
@@ -5,27 +5,20 @@ import matplotlib.dates as mdates
 from matplotlib import dates as mdates
 import math
 from datetime import datetime as dt
-def allanvar_conversion_counter(startPoint,timeInterval):   #連続した3点を取れたらカウントする関数
-    nextPoint=np.where(scaledTime[startPoint]+timeInterval==scaledTime)[0]
-    thirdPoint=np.where(scaledTime[startPoint]+timeInterval*2==scaledTime)[0]
-    if nextPoint and thirdPoint :
-       return 1
-    else:
-       return 0
-def Total_threePoints(startPoint,timeInterval):  #連続した3点を足し合わせアラン分散を計算する関数
-    nextPoint=np.where(scaledTime[startPoint]+timeInterval==scaledTime)[0]
-    thirdPoint=np.where(scaledTime[startPoint]+timeInterval*2==scaledTime)[0]
-    y1=XYimagPart[startPoint]; y2=XYimagPart[int(nextPoint)]; y3=XYimagPart[int(thirdPoint)]
-    threePoints=(y3-2*y2-y1)**2
-    return threePoints
-def allanvar_graph(): #グラフをプロット
-    X=TI
-    Y=allanvariance
-    plt.loglog(X,Y,"ro",markersize=3)
+def allanvar_conversion_counter(startPoint, timeInterval, integerTimeCode):
+    nextPoint = np.where( integerTimeCode[startPoint] + timeInterval == integerTimeCode)[0]
+    thirdPoint = np.where( integerTimeCode[startPoint] + timeInterval*2 == integerTimeCode)[0]
+    return np.array([startPoint]), nextPoint, thirdPoint
+
+def threePointVar(timeIndex, measuredValue):  
+    return (measuredValue[timeIndex[0]] - 2*measuredValue[timeIndex[1]] + measuredValue[timeIndex[2]])**2
+
+def allanvar_graph(arranArray,timeInterval): #グラフをプロット
+    plt.loglog(timeInterval,arranArray,"ro",markersize=3)
     plt.title('G31.41+0_a_06_TE/Xb7d0ee BB1')
     plt.xlabel("time lag [s]")
     plt.ylabel("Allan variance")
-    a=np.array([  2,   10,  100, 1000],dtype=float)
+    a=np.array([  1,   10,  100, 1000, 10000],dtype=float)
     b=5.8e-6
     plt.loglog(a,a**(-2)*b,'k')
 
@@ -34,23 +27,19 @@ scaledTime = np.round((timeStamp-np.min(timeStamp))/np.min(np.diff(timeStamp)))
 dataSize=len(timeStamp)
 XYimagPart=np.imag(np.load('BB1-SPW0-DA59.XYC.npy'))[0,0:dataSize]
 TotalScanTime=np.int(scaledTime[dataSize-1]-scaledTime[0])
-
-
+allanvarianceArray=np.array([]);timeIntervalArray=np.array([])
 for TI in range(1,TotalScanTime):  #次点の決定
           Total=0
-          N=0
+          num_threePointVariance=0
           for SP in range(1,dataSize-2): #始点の決定
-              if allanvar_conversion_counter(SP,TI) == 1:
-                 Total+=Total_threePoints(SP,TI)
-                 N+=allanvar_conversion_counter(SP,TI)
-             #    print(TI,Total,N,SP)
-              else:
-                 pass
-          if not N ==0:
-             allanvariance=(Total/N/TI**2)/2
-             print(TI,SP,allanvariance) 
-             allanvar_graph()
-          else:
-             pass
-    
+              if allanvar_conversion_counter(SP, TI, scaledTime)[1] and allanvar_conversion_counter(SP, TI, scaledTime)[2]:
+                 conter=allanvar_conversion_counter(SP, TI, scaledTime)
+                 timeIndex_list=np.hstack((conter[0],conter[1],conter[2])).tolist()
+                 Total+=threePointVar(timeIndex_list, XYimagPart)
+                 num_threePointVariance+=1
+          if num_threePointVariance > 1:
+             allanvariance=(Total/num_threePointVariance/TI**2)/2
+             allanvarianceArray=np.append(allanvarianceArray, allanvariance)
+             timeIntervalArray=np.append(timeIntervalArray,TI)
+allanvar_graph(allanvarianceArray,timeIntervalArray)
 
