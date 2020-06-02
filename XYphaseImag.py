b01799e3c4f1dc910355a83bfd9bdfaeecb31766
YukiEtoh <snowy0208@icloud.com>
2020-06-03 00:54:24 +0900

    XYphaseImag.py

--- XYphaseImag.py
+++ XYphaseImag.py
@@ -5,12 +5,13 @@ import matplotlib.dates as mdates
 from matplotlib import dates as mdates
 import math
 from datetime import datetime as dt
-
+FileName='G31.41+0_a_06_TE_Xb7d0ee_X63d5'
+AntenaNumber='DA59'
 for BB_index in range(4):
     BBlist = 'BB' + `BB_index + 1`
-    prefix = BBlist + '-SPW0-DA59'  #PutTheFileName
+    prefix = BBlist + '-SPW0-'+AntenaNumber  #PutTheFileName
     XYC = np.imag(np.load(prefix+'.XYC.npy'))
-    Y= XYC[0,0:840]
+    Y= XYC[0,0:1200]
     TS = np.load(prefix+'.TS.npy')
     def time_convert(mytime,myunit='s'):
         if type(mytime).__name__ <>'list': mytime=TS
@@ -28,13 +29,13 @@ for BB_index in range(4):
     def plot():
         plt.gca().xaxis.set_major_locator(mdates.AutoDateLocator())
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%-m/%-d %-H:%M"))
-        plt.title('XYphase stability ')
+        plt.title('XYphase stability'+'_'+FileName+'_'+AntenaNumber)
         plt.legend(loc='upper right')
         plt.xlabel("time")
         plt.ylabel("XY imaginary part")
         plt.grid()
         plt.show()
-        plt.savefig('XYphaseImag_'+BBlist+'.png')
+        plt.savefig(FileName +'XYphaseImag_'+BBlist+'.png')
         plt.close()
     if BB_index == 0:
        plt.plot(X,Y,"ro" ,label=BBlist)
