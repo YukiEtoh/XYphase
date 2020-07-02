7854bc567c1f61ca053e250c616212a4c9f94c0b
YukiEtoh <snowy0208@icloud.com>
2020-07-02 16:48:27 +0900

    add new file

--- allan_hist.py
+++ allan_hist.py
@@ -10,7 +10,7 @@ Band='Band3'
 SessionList=['Xb24884_X94f','Xb88fca_X10091','Xb7189f_Xa9cb','Xb8c0d3_X1053','Xb87877_X23c0','Xb945f7_X2f40','Xb903d6_X24ff','Xc20718_X3af7','Xc7111c_X8795','Xb9dfa4_X566e']
 BaseBand='BB4'
 direc='/Users/etohyuki/Desktop/XYPhaseRecalibratedData/'+Band+'/'
-TimeRange=range(1000,1200)
+TimeRange=[1320]
 def allanvar_conversion_counter(startPoint, timeInterval, integerTimeCode):  #return consecutive threepoints
     nextPoint = np.where( integerTimeCode[startPoint] + timeInterval == integerTimeCode)[0]
     thirdPoint = np.where( integerTimeCode[startPoint] + timeInterval*2 == integerTimeCode)[0]
@@ -19,13 +19,17 @@ def threePointVar(timeIndex, measuredValue):  #calculate three points
     return (measuredValue[timeIndex[0]] - 2*measuredValue[timeIndex[1]] + measuredValue[timeIndex[2]])**2
 def allanvarhist(allanVar,Frequency):
     plt.hist(allanVar)
-    plt.savefig('HistAllanvarAllsession'+Band+'.png')
-    plt.xlabel("AllanVariance_\sigma^2")
+    plt.savefig('HistAllanvar'+Band+'.png')
+    plt.xlabel("AllanVariance[dex]")
     plt.ylabel("Frequency_TotalData")
     plt.close()
 def flatten(nested_list): 
     return [e for inner_list in nested_list for e in inner_list]
 
+def getNearestValue(samplevalue, anynumber):
+    idx = np.abs(np.asarray(samplevalue) - anynumber).argmin()
+    return samplevalue[idx]
+any_allan=[]
 for Session in range(0,len(SessionList)):
     files=direc+SessionList[Session]+"/NPY/"
     timeStamp=np.load(glob.glob(files+BaseBand+'*'+'D'+'*TS.npy')[0])
@@ -48,10 +52,12 @@ for Session in range(0,len(SessionList)):
            allanvarianceList = allanvarianceList + [allanvariance]
            timeIntervalList = timeIntervalList + [TI]
     allan_time=[flatten(allanvarianceList),timeIntervalList] #CreatePairOfAllan&Time
-    U = list(set(TimeRange)&set(allan_time)[0]
-    'allan_'+str(Session).index(U)
+#    exec('allan_'+str(Session) + '=[flatten(allanvarianceList),(timeIntervalList)]') #CreatePairOfAllan&Time
 
-
+    anyAllanIndex=allan_time[1].index(getNearestValue(allan_time[1],TimeRange))
+    print(getNearestValue(allan_time[1],TimeRange))
+    any_allan.append(allan_time[0][anyAllanIndex])
+allanvarhist(any_allan,len(SessionList))
 #allanvarhist(np.log10(allanvarianceList),len(SessionList))
 #    print(np.array(int(math.log10(allanvarianceList))),Session)
-
+ 
